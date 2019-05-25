bd12fe0a4b3d059502eacddbac175371bed3f346
RaiRaiRobin <rai.ribin1000@gmail.com>
2019-05-25 21:45:04 +0545

    Splash Screen fixed

--- app/src/main/java/com/example/finalandroidassignmentproject/LoginActivity.java
+++ app/src/main/java/com/example/finalandroidassignmentproject/LoginActivity.java
@@ -1,13 +1,32 @@
 package com.example.finalandroidassignmentproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText loginUsername, loginPassword;
+    private TextView register;
+    private Button signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginUsername = findViewById(R.id.loginUserame);
+        loginPassword = findViewById(R.id.loginPassword);
+        register = findViewById(R.id.register);
+        signIn = findViewById(R.id.signIn);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
     }
 }
